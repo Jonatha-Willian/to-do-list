d25467e8abfc6a215dfc6c99b9fe2b2ee677190a
Jonatha Willian P Santos <jonathasantos.dev@hptmail.com>
2026-01-28 10:07:30 -0300

    Add new task on DB

--- src/controllers/to-do.controller.ts
+++ src/controllers/to-do.controller.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from 'express';
-import { ToDo } from '../models/to-do';
+import { ToDo } from '../models/to-do.model';
+import { error } from 'console';
 
-export const all = async () => {
-    //Lógica para pegar todas as tarefas
+export const all = async (req: Request, res: Response) => {
+    //Usand o o model ToDo para buscar todas as tarefas no banco de dados
+    const list = await ToDo.findAll();
+    res.json({ list });
 }
-export const add = async () => {
-    //Lógica para criar uma nova tarefa
+export const add = async (req: Request, res: Response) => {
+    if(req.body.title) {
+        let newTask = await ToDo.create({
+            title: req.body.title,
+            //se o valor enviado for "true", atribui true, senão atribui false a done
+            done: req.body.done === "true" ? true : false
+        });
+        res.status(201).json({ message: "Tarefa adicionada!", item: newTask });
+    }else{
+        res.json({ error: 'dados não enviados' });
+    }
 }
-export const update = async () => {
+export const update = async (req: Request, res: Response) => {
     //Lógica para atualizar uma tarefa existente
 }
-export const remove = async () => {
+export const remove = async (req: Request, res: Response) => {
     //Lógica para deletar uma tarefa existente
 }
